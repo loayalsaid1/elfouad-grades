18203b69d0362810e0bebdd3a000d05000a66230
Loay Al-Said <l.programming555@gmail.com>
2025-05-30 21:06:47 +0300

    Allow the contatns file in the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,16 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}"
+    "*.{js,ts,jsx,tsx,mdx}",
+		"/constants/*.{js,ts,jsx,tsx,mdx}",
   ],
+	//   safelist: [
+  //   "bg-blue-100", "text-blue-800", "border-blue-200",
+  //   "bg-green-100", "text-green-800", "border-green-200",
+  //   "bg-yellow-100", "text-yellow-800", "border-yellow-200",
+  //   "bg-red-100", "text-red-800", "border-red-200",
+  //   "bg-gray-100", "text-gray-800", "border-gray-200",
+  // ],
   theme: {
   	extend: {
   		colors: {
